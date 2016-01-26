fabf0874a5d8a727beecab1873b33f632ba74e45
mengguoru <2506839494@qq.com>
2016-01-26 18:28:40 +0800

    add test_Delegate project

---
+++ testDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public delegate void greeting(String name);
+    class Program
+    {
+        private static void greetingPeople(greeting func, String name)
+        {
+            func(name);
+        }
+        private static void ChineseGreeting(String name)
+        {
+            Console.WriteLine("早上好" + name);
+        }
+        private static void EnglishGreeting(String name)
+        {
+            Console.WriteLine("Good morning" + name);
+        }
+        static void Main(string[] args)
+        {
+            greetingPeople(ChineseGreeting, "中文名字");
+            greetingPeople(EnglishGreeting, "English name");
+        }
+    }
+}
