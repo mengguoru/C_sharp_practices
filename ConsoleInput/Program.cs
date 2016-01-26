5f988d3c69eb9103f42a4ee3ef27e2bfcddebc26
mengguoru <2506839494@qq.com>
2016-01-26 10:50:08 +0800

    add ConsoleInput project

---
+++ ConsoleInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input:");
+            string s = Console.ReadLine();
+            Console.WriteLine("You input:{0}", s);
+        }
+    }
+}
